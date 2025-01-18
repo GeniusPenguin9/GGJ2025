7ae9498182a41fd0b5ef5d12a686539e29bf922f
GeniusPenguin9 <emily@caulyemily.info>
2025-01-18 11:14:11 +0800

    Add monster

---
+++ BubbleGame/Assets/Scripts/Monster.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Monster : MonoBehaviour
+{
+    public Color[] _colors;
+    public int _score;
+    public SpriteRenderer _spriteRenderer;
+
+    public void InitialMonster(Color[] colors, int score, SpriteRenderer spriteRenderer){
+        this._colors = colors;
+        this._score = score;
+        this._spriteRenderer = spriteRenderer;
+    }
+}
+
+public enum Color
+{
+    Red,
+    Green,
+    Blue,
+    Yellow
+}
+
